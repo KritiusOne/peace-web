d24d6858ac06795df94dc7e1861f92284d82b6f8
Julio Rodriguez Ureche <julio10489@gmail.com>
2025-11-28 21:15:45 -0500

    feat: add colors on ts format

---
+++ lib/Colors.ts
@@ -0,0 +1,59 @@
+const Colors = {
+  // Green Colors
+  GREEN: {
+    100: "#84ebb4",
+    200: "#1fc16b",
+    ALPHA_10: "rgba(31, 193, 107, 0.1)",
+  },
+
+  // Neutral Colors
+  NEUTRAL: {
+    100: "#fff",
+    200: "#e8e8e8",
+    300: "#d2d2d2",
+    400: "#bbb",
+    500: "#a4a4a4",
+    600: "#8e8e8e",
+    700: "#777",
+    800: "#606060",
+    900: "#4a4a4a",
+    1000: "#333",
+    ALPHA_10: "rgba(51, 51, 51, 0.1)",
+  },
+
+  // Primary Colors
+  PRIMARY: {
+    100: "#c9e1e2",
+    200: "#92c2c6",
+    300: "#5ca4a9",
+    400: "#468084",
+    500: "#315b5e",
+    ALPHA_10: "rgba(92, 164, 169, 0.1)",
+  },
+
+  // Red Colors
+  RED: {
+    100: "#fb3748",
+    200: "#d00416",
+    ALPHA_10: "rgba(251, 55, 72, 0.1)",
+  },
+
+  // Secondary Colors
+  SECONDARY: {
+    100: "#ffedd5",
+    200: "#fed7aa",
+    300: "#feb973",
+    400: "#fc913b",
+    500: "#fa6e0f",
+    ALPHA_10: "rgba(250, 109, 15, 0.1)",
+  },
+
+  // Yellow Colors
+  YELLOW: {
+    100: "#ffdb43",
+    200: "#dfb400",
+    ALPHA_10: "rgba(255, 219, 67, 0.1)",
+  },
+} as const;
+
+export default Colors;
